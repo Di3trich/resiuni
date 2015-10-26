a5d3ec56e3543425c653cb72a180e3495f6d1fbe
Di3trich <qf.jonathan@gmail.com>
2015-10-26 06:06:31 -0500

    json

--- residencias/views.py
+++ residencias/views.py
@@ -3,6 +3,8 @@ from django.template import Context
 from django.shortcuts import render
 from models import Sede, Institucion
 
+import json
+
 def show_main(request):
     sede = Sede.objects.get(id = 1)
     c = Context()
@@ -10,6 +12,16 @@ def show_main(request):
     #institucion = Institucion.objects.get(id = sede['institucion'])
     #c['institucion'] = institucionx
     return render(request, 'main.html' , c)
+
+def another_main(request):
+    sede = Sede.objects.get(id = 1)
+    data={}
+    data['latitude']=sede.latitude
+    data['longitude']=sede.longitude
+    data['id']=sede.id
+    return HttpResponse(json.dumps(data), content_type="application/json")
+
+
 
 
 # Create your views here.
